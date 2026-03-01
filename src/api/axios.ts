49af25c1d43a5555b3b72eeba1461acfb9cfbc18
rahulharlapur <harlapur25@gmail.com>
2026-03-01 15:31:46 +1000

    Use env variable for API URL

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useAuthStore } from "@/store/authStore";
 
-const BASE_URL = "http://192.168.0.55:3000/v1";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/v1";
 
 export const api = axios.create({
   baseURL: BASE_URL,
